ed43a559ed5a070204fe1a7d35222dd8d259c3ba
Joseph <chetra.pang.2064319@gmail.com>
2024-12-16 10:36:28 +0700

    add model.dart

---
+++ lib/models/model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class models extends StatelessWidget {
+  const models({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
